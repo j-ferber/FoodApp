8110669bff50123038aa66ebb050c4c06f517464
j-ferber <ferberjustin5@gmail.com>
2023-07-08 01:00:34 -0400

    responsive food

--- foodApp/src/Nav.jsx
+++ foodApp/src/Nav.jsx
@@ -1,13 +1,17 @@
-import React, { useState, useContext } from 'react'
-import { FaSearch } from 'react-icons/fa'
+import React, { useContext, useState } from 'react'
+import { FaSearch, FaBars } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import DataContext from './context/DataContext'
+import { useWindowSize } from 'react-use'
+import { BiX } from "react-icons/bi"
 
 const Nav = ({ search, setSearch }) => {
-  const { setSearchData, setLoading, setFetchError } = useContext(DataContext)
+  const { setSearchData, setLoading } = useContext(DataContext)
   const navigate = useNavigate()
+  const { width } = useWindowSize()
+  const [hamClicked, setHamClicked] = useState(false)
   
   const handleSearch = async (e) => {
     e.preventDefault()
@@ -20,15 +24,37 @@ const Nav = ({ search, setSearch }) => {
 
   return (
     <nav className='navRow'>
-      <form onSubmit={handleSearch}>
-        <input id='search' type="text" placeholder='Enter a meal' value={search} onChange={(e) => setSearch(e.target.value)} />
-        <FaSearch tabIndex="0" className='searchButton' onClick={handleSearch} style={{cursor: 'pointer'}}/>
-      </form>
-      <ul>
-        <li><Link to='/'>Home</Link></li>
-        <li><Link to='about'>About</Link></li>
-        <li><Link to='random'>Random</Link></li>
-      </ul>
+      {width >= 430 ? (
+        <>
+          <form onSubmit={handleSearch}>
+            <input id='search' type="text" placeholder='Enter a meal' value={search} onChange={(e) => setSearch(e.target.value)} />
+            <FaSearch tabIndex="0" className='searchButton' onClick={handleSearch} style={{cursor: 'pointer'}}/>
+          </form>
+          <ul>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='about'>About</Link></li>
+            <li><Link to='random'>Random</Link></li>
+          </ul>
+        </>
+      ) : !hamClicked ? (
+        <>
+          <form onSubmit={handleSearch}>
+            <input id='search' type="text" placeholder='Enter a meal' value={search} onChange={(e) => setSearch(e.target.value)} />
+            <FaSearch tabIndex="0" className='searchButton' onClick={handleSearch} style={{cursor: 'pointer'}}/>
+          </form>
+          <FaBars className='menu' onClick={() => setHamClicked(true)}/>   
+        </>
+      ) : (
+        <>
+          <ul className='hamburgerList'>
+            <li className='navWord'><Link to='/'>Home</Link></li>
+            <li className='navWord'><Link to='about'>About</Link></li>
+            <li className='navWord'><Link to='random'>Random</Link></li>
+          </ul>
+          <BiX className='iconX' onClick={() => setHamClicked(false)} />
+        </>   
+      )
+    } 
     </nav>
   )
   
